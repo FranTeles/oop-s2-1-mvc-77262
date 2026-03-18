ecbb7a450fe3b718134968587c558845550580bf
Fran Teles <dcollege.77262@gmail.com>
2026-03-18 11:03:17 +0000

    Implemented Library system: models, CRUD, loans, return feature and search/filter

---
+++ Library.Domain/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+
+        [Required]
+        public string Isbn { get; set; } = string.Empty;
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        public bool IsAvailable { get; set; } = true;
+
+        public List<Loan> Loans { get; set; } = new List<Loan>();
+    }
+}
---
+++ Library.Domain/Member.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Domain
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public List<Loan> Loans { get; set; } = new List<Loan>();
+    }
+}
